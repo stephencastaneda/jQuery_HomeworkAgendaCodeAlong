2444f9feca9a4b73ca2f512282fd7eb3f9fed5c0
stephencastaneda <59769978+stephencastaneda@users.noreply.github.com>
2021-04-07 10:35:13 -0500

    changes css import

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import "./styles.css";
 import $ from "jquery";
+
+$("li:lt(4)").hide().fadeIn(1500);
+
+$("li").on("click", function () {
+  $(this).remove();
+});
+
+$("#center").on("click", function () {
+  $("body").addClass("center");
+});
+
+$("#dark").on("click", function () {
+  $("body").toggleClass("dark");
+});
